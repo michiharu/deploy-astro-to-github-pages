fedc5e4acd312c98080b0b7f0f0b9af2d5c0543b
michiharu <michi1diver@gmail.com>
2023-09-20 03:41:25 +0900

    :wrench: update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+  site: 'https://michiharu.github.io',
+  base: '/deploy-astro-to-github-pages',
+  trailingSlash: 'always',
 	integrations: [mdx(), sitemap()],
 });
---
+++ src/funcs.ts
@@ -0,0 +1,11 @@
+/**
+ * ```typescript
+ * url('/blog/') => '/my-repo/blog/'
+ * url('/image.png') => '/my-repo/image.png'
+ * ```
+ */
+export const url = (path: string | undefined) => {
+  if (path === undefined) return undefined;
+  const base = import.meta.env.BASE_URL.replace(/\/$/, '');
+  return `${base}${path}`;
+};
